16f4541447b5fcf7a4fedbc9292fa2c7da4e126c
Nejc Ribic <nejc.ribic95@gmail.com>
2017-10-05 14:40:04 +0200

    contact layout 2/4

--- javascript/main.js
+++ javascript/main.js
@@ -1,47 +1,12 @@
 function initMap() {
-    var uluru = {lat: -25.363, lng: 131.044};
+
+    var uluru = {lat: 46.170911, lng: 14.434540};
     var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 4,
+        zoom: 12,
         center: uluru
     });
     var marker = new google.maps.Marker({
         position: uluru,
         map: map
     });
-}
-
-// var textarea = document.getElementById("ta");
-// var heightLimit = 200; /* Maximum height: 200px */
-//
-// textarea.oninput = function() {
-//     console.log("test");
-//     textarea.style.height = ""; /* Reset the height*/
-//     textarea.style.height = Math.min(textarea.scrollHeight, heightLimit) + "px";
-// };
-// var textarea = document.getElementById("ta");
-// var limit = 200;
-// var label = document.getElementById("ta-label");
-//
-//
-// textarea.oninput = function() {
-//     textarea.style.height = "";
-//     textarea.style.height = (Math.min(textarea.scrollHeight, 300)) + "px";
-// };
-
-// $(function() {
-//     $("ta").autoResize()
-// });
-
-var textarea = document.querySelector('textarea');
-
-textarea.addEventListener('keydown', autosize);
-
-function autosize(){
-    var el = this;
-    setTimeout(function(){
-        // el.style.cssText = 'height:auto; padding:0';
-        // for box-sizing other than "content-box" use:
-        // el.style.cssText = '-moz-box-sizing:content-box';
-        el.style.cssText = 'height:' + el.scrollHeight + 'px';
-    },0);
 }
