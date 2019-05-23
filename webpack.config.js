cb974ddcc553d1e6c74935edf68f3d63391bd796
Nejc Ribic <nejc.ribic95@gmail.com>
2019-05-23 22:00:35 +0200

    webpack integration part 1

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,38 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     entry: './src/javascript/main.js',
     output: {
-        filename: 'main.js',
+        filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
+    plugins: [
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            filename: '[name].css',
+            chunkFilename: '[id].css',
+            path: path.resolve(__dirname, 'dist')
+        }),
+    ],
     module: {
         rules: [
             {
                 test: /\.css$/,
                 use: [
-                    'style-loader',
-                    'css-loader'
-                ]
-            }
-        ]
-    }
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            // you can specify a publicPath here
+                            // by default it uses publicPath in webpackOptions.output
+                            publicPath: 'dist',
+                            hmr: process.env.NODE_ENV === 'development',
+                        },
+                    },
+                    'css-loader',
+                ],
+            },
+        ],
+    },
 };
